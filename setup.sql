672862b52639615d99585058da0460759d537086
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-08-06 15:35:05 -0500

    good to go

--- setup.sql
+++ setup.sql
@@ -1,5 +1,6 @@
 # Below create the user we will be using to access the data for the application
-CREATE USER audiospace_user@localhost IDENTIFIED BY '1q2w3e4r!Q@W#E$R';
+# TODO: change user password to what you are going to use, don't use default
+CREATE USER audiospace_user@localhost IDENTIFIED BY 'password';
 # Below grants the user we just created all CRUD functionality on our new database.
 GRANT ALL ON audiospace_db.* TO audiospace_user@localhost;
 #  Below drops the database if it is already create (You will lose all previous data)

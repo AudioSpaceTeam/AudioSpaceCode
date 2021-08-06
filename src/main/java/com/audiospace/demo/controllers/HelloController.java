a9d0ecd711fa8cbc7b64dc22de21705ccc0ec1d1
Alejandro Cabrera <cabrera.alejandr26@gmail.com>
2021-08-06 14:36:08 -0500

    added folders

--- src/main/java/com/audiospace/demo/controllers/HelloController.java
+++ src/main/java/com/audiospace/demo/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.audiospace.demo;
+package com.audiospace.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;

fd814ecfb136188d6e9d338165ea483f0f9e625d
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-08-07 17:14:55 -0500

    doesn't work yet don't combine

--- src/main/java/com/audiospace/demo/models/Event.java
+++ src/main/java/com/audiospace/demo/models/Event.java
@@ -2,6 +2,7 @@ package com.audiospace.demo.models;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "events")
@@ -31,13 +32,128 @@ public class Event {
 //  Event flyer image
 //  Not yet...
 
-//  DATETIME DATETIME
+  //  DATETIME DATETIME
   @Column(nullable = false)
   private Date datetime;
 
-//  price double
-@Column(nullable = false)
+  //  price double
+  @Column(nullable = false)
   private Double price;
+
   //  Promoter ID FK
+  // Establishes that there's going to be multiple events tied back to One user.
+  // Join column binds the relationship together?
+  @ManyToOne
+  @JoinColumn(name = "promoter_id")
+  private User promoter;
+
+  public Event(long id, String title, String description, Integer slots, String location, Date datetime, Double price, User promoter) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.slots = slots;
+    this.location = location;
+    this.datetime = datetime;
+    this.price = price;
+    this.promoter = promoter;
+  }
+
+  public Event(String title, String description, Integer slots, String location, Date datetime, Double price, User promoter) {
+    this.title = title;
+    this.description = description;
+    this.slots = slots;
+    this.location = location;
+    this.datetime = datetime;
+    this.price = price;
+    this.promoter = promoter;
+  }
+
+  public Event( String title, String description, Integer slots, String location, Date datetime, Double price) {
+    this.title = title;
+    this.description = description;
+    this.slots = slots;
+    this.location = location;
+    this.datetime = datetime;
+    this.price = price;
+  }
+
+  public Event(long id, String title, String description, Integer slots, String location, Date datetime, Double price) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.slots = slots;
+    this.location = location;
+    this.datetime = datetime;
+    this.price = price;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Integer getSlots() {
+    return slots;
+  }
+
+  public void setSlots(Integer slots) {
+    this.slots = slots;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  public Date getDatetime() {
+    return datetime;
+  }
+
+  public void setDatetime(Date datetime) {
+    this.datetime = datetime;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public User getPromoter() {
+    return promoter;
+  }
+
+  public void setPromoter(User promoter) {
+    this.promoter = promoter;
+  }
+
+  //  bands sloted?
+//    @ManyToMany(mappedBy = "performers")
+//    private List<User> performers;
+
 
 }
--- src/main/java/com/audiospace/demo/models/User.java
+++ src/main/java/com/audiospace/demo/models/User.java
@@ -1,17 +1,19 @@
 package com.audiospace.demo.models;
 
 import javax.persistence.*;
+import java.util.List;
+
 // Below allows us to the Dependency for creating our users table dynamically without needed to insert it into the console.
 @Entity
 @Table(name = "users")
 public class User {
-//  This ID is going to be the MAIN identifier, that is in this class.
+  //  This ID is going to be the MAIN identifier, that is in this class.
 //  Database understands it will be auto incremented inside of mysql.
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-//  Below sets up parameters for our column in the table in the DB.
+  //  Below sets up parameters for our column in the table in the DB.
   @Column(nullable = false, length = 25)
   private String username;
 
@@ -21,19 +23,61 @@ public class User {
   @Column(nullable = false)
   private String displayName;
 
-//  Password
+  //  Password
   @Column(nullable = false)
   private String password;
 
 //  Profile Image? -- we can add later.
 
-//  Bio
+  //  Bio
   @Column(nullable = false, length = 500)
   private String bio;
 
-//  Is promoter bool
+  //  Is promoter bool
   @Column(nullable = false)
   private Boolean isPromoter;
+
+  // Sets the events to the promoter
+//  Below says 1 user to many events...
+//  Cascade is when the user gets deleted, all of it's associated ads would also get deleted.
+//  We are then mapping this under the user
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+  private List<Event> promotedEvents;
+//  Above is now a reference to all the events the user should have associated with it?
+
+
+  public User(long id, String username, String email, String displayName, String password, String bio, Boolean isPromoter, List<Event> promotedEvents) {
+    this.id = id;
+    this.username = username;
+    this.email = email;
+    this.displayName = displayName;
+    this.password = password;
+    this.bio = bio;
+    this.isPromoter = isPromoter;
+    this.promotedEvents = promotedEvents;
+  }
+
+  public List<Event> getPromotedEvents() {
+    return promotedEvents;
+  }
+
+  public void setPromotedEvents(List<Event> promotedEvents) {
+    this.promotedEvents = promotedEvents;
+  }
+
+
+  //  Set performers to events
+//    @ManyToMany(cascade = CascadeType.ALL)
+//    @JoinTable(
+//      name="user_events",
+//      joinColumns={@JoinColumn(name="user_id")},
+//      inverseJoinColumns={@JoinColumn(name="event_id")}
+//    )
+//    private List<Event> performEvents;
+
+//  Reviews
+
+//  Genres
 
 
   public long getId() {

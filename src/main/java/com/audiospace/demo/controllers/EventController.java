8498fec6f55b29bb7c38e66b11ba1158286ca4f2
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-08-09 16:59:56 -0500

    alejandro stuff

--- src/main/java/com/audiospace/demo/controllers/EventController.java
+++ src/main/java/com/audiospace/demo/controllers/EventController.java
@@ -4,37 +4,32 @@ import com.audiospace.demo.models.Event;
 import com.audiospace.demo.repositories.EventRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class EventController {
-    private final EventRepository eventDao;
+  private final EventRepository eventDao;
 
-    public EventController(EventRepository eventDao){
-        this.eventDao = eventDao;
-    }
+  public EventController(EventRepository eventDao){
+    this.eventDao = eventDao;
+  }
 
-    @GetMapping("/event/create")
-    public String createAd(Model model) {
-        model.addAttribute("event",eventDao.findAll());
-        return "event/create";
-    }
+  @GetMapping("/event/create")
+  public String createAd(Model model) {
+    model.addAttribute("event",new Event());
+    return "event/create";
+  }
 
-    @GetMapping("/event/submitted")
-    public String formSubmitted(){
-        return "event/submitted";
-    }
-//    @PostMapping("/event/create")
-//    public String saveCreate(@RequestParam(name = "title") String title,
-//        @RequestParam(name="location")String location,
-//        @RequestParam(name="description")String description){
-//            EventModel event = new EventModel();
-//            event.setTitle(title);
-//            event.setLocation(location);
-//            event.setDescripion(description);
-//            return "redirect:/event/submitted";
-//    }
+  @GetMapping("/event/submitted")
+  public String formSubmitted(){
+    return "event/submitted";
+  }
+
+  @PostMapping("/event/create")
+  public String saveCreate(@ModelAttribute Event event){
+    event.setPrice(5.00);
+    event.setSlots(4);
+    eventDao.save(event);
+    return "redirect:/event/submitted";
+  }
 }

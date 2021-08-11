41c789ced19710c05e71d863d162312e2764b1c3
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-08-11 16:27:12 -0500

    added show and index page

--- src/main/java/com/audiospace/demo/controllers/EventController.java
+++ src/main/java/com/audiospace/demo/controllers/EventController.java
@@ -1,5 +1,7 @@
 package com.audiospace.demo.controllers;
+
 import java.util.Random;
+
 import com.audiospace.demo.models.Event;
 import com.audiospace.demo.repositories.EventRepository;
 import com.audiospace.demo.models.User;
@@ -17,32 +19,45 @@ import java.util.List;
 
 @Controller
 public class EventController {
-  private final EventRepository eventDao;
-  private final UserRepository userDao;
+    private final EventRepository eventDao;
+    private final UserRepository userDao;
 
-  public EventController(EventRepository eventDao, UserRepository userDao){
-    this.eventDao = eventDao;
-    this.userDao = userDao;
-  }
+    public EventController(EventRepository eventDao, UserRepository userDao) {
+        this.eventDao = eventDao;
+        this.userDao = userDao;
+    }
 
+    //added show an view events
+    @GetMapping("/event")
+    public String viewEvent(Model model) {
+        model.addAttribute("event", eventDao.findAll());
+        return "event/index";
+    }
 
-@GetMapping("/event/create")
-  public String createEvent(Model model){
-    model.addAttribute("event", new Event());
-    return "/event/create";
-}
+    @GetMapping("/event/{id}")
+    public String singleEvent(@PathVariable long id, Model model) {
+        Event event = eventDao.getById(id);
+        model.addAttribute("event", event);
+        return "event/show";
+    }
 
-  @PostMapping("/event/create")
-  public String saveCreate(@RequestParam(name="startDateTime") String startDateTime, @RequestParam(name="endDateTime") String endDateTime,@ModelAttribute Event event, Model model) {
-    User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    event.setPromoter(currentUser);
+    @GetMapping("/event/create")
+    public String createEvent(Model model) {
+        model.addAttribute("event", new Event());
+        return "/event/create";
+    }
 
-    event.setStartDateTime(LocalDateTime.parse(startDateTime));
-    event.setEndDateTime(LocalDateTime.parse(endDateTime));
-    eventDao.save(event);
-    model.addAttribute("event", event);
-    return "/event/submitted";
-}
+    @PostMapping("/event/create")
+    public String saveCreate(@RequestParam(name = "startDateTime") String startDateTime, @RequestParam(name = "endDateTime") String endDateTime, @ModelAttribute Event event, Model model) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        event.setPromoter(currentUser);
+
+        event.setStartDateTime(LocalDateTime.parse(startDateTime));
+        event.setEndDateTime(LocalDateTime.parse(endDateTime));
+        eventDao.save(event);
+        model.addAttribute("event", event);
+        return "/event/submitted";
+    }
 
 
 }

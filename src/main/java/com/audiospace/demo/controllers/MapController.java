25d0127419b5ca68201412dfa45e2a3f77167ea0
Savanna Marie <savannamarie@gmail.com>
2021-08-12 11:38:28 -0500

    Working map now showing events

--- src/main/java/com/audiospace/demo/controllers/MapController.java
+++ src/main/java/com/audiospace/demo/controllers/MapController.java
@@ -1,15 +1,26 @@
 package com.audiospace.demo.controllers;
 
+import com.audiospace.demo.models.Event;
 import com.audiospace.demo.models.User;
+import com.audiospace.demo.repositories.EventRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class MapController {
+    private final EventRepository eventDao;
+    public MapController(EventRepository eventDao){
+        this.eventDao = eventDao;
+
+    }
+
     @GetMapping("/map")
     public String showSignupForm(Model model) {
-        model.addAttribute("user", new User());
+        List<Event> eventsList = eventDao.findAll();
+        model.addAttribute("eventList", eventsList);
         return "map";
     }
 }

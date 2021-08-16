26c8159554c28e43d7bbdd0778e9401f91f15f0c
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-08-16 15:13:01 -0500

    search works

--- src/main/java/com/audiospace/demo/repositories/EventRepository.java
+++ src/main/java/com/audiospace/demo/repositories/EventRepository.java
@@ -3,8 +3,12 @@ package com.audiospace.demo.repositories;
 import com.audiospace.demo.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventRepository extends JpaRepository<Event, Long> {
   Event findById(long id);
+
+  List<Event> findAllByTitleContainingOrDescriptionContaining(String title, String description);
 
   void deleteById(long id);
 
